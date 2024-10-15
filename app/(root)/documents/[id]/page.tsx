099909dcb5c98fcefa55e4f9e079372e3301427f
Joeri <56969837+JoeriVeugelen@users.noreply.github.com>
2024-10-15 10:59:14 +0200

    Added collaborative room create button

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,6 +1,3 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const Document = () => {
